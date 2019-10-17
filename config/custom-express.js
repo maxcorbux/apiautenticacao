a2a52bbb31271278496e9a7c2dd5b12c431ca5d5
Max Hiroito <maxhiroito@hotmail.com]>
2019-10-17 11:49:20 -0400

    aula 2

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,6 +1,12 @@
 let express = require("express");
+let consign = require('consign');
 
 module.exports = () => {
     let app = express();
+
+    consign()
+        .include('rotas')
+        .into(app);
+
     return app;
 }
--- rotas/pagamentos.js
+++ rotas/pagamentos.js
@@ -1,6 +1,11 @@
-module.exports(app) {
+module.exports = function(app) {
     app.get('/pagamentos', function(req, res) {
-        console.log('')
-        res.send("ok")
+        console.log('');
+        res.send("ok");
+    });
+    app.post('/pagamentos/pagamento', function(req, res) {
+        let pagamento = req.body;
+        console.log(pagamento);
+        res.send("Ok");
     });
 }
