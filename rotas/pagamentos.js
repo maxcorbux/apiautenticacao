e95c7c2870d173d7a1e058f5cb38f98f739de657
Max Hiroito <maxhiroito@hotmail.com]>
2019-10-23 11:17:09 -0400

    teste Branch

--- rotas/pagamentos.js
+++ rotas/pagamentos.js
@@ -1,25 +1,25 @@
 module.exports = function(app) {
-    app.get('/pagamentos', function(req, res) {
-        console.log('');
-        res.send("ok");
-    });
-    app.post('/pagamentos/pagamento', function(req, res) {
-        let pagamento = req.body;
-        console.log("processano um novo pagamento");
-
-        let connection = app.persistencia.connectionFactory();
-        let pagamentoDao = new app.persistencia.PagamentoDao(connection);
-
-        pagamento.status = "CRIADO"
-        pagamento.data = new Date();
-
-        pagamentoDao.salva(pagamento, function(erro, resultado) {
-            console.log('pagamento criado ' + resultado);
-            console.log(erro)
-            res.json(pagamento);
+        app.get('/pagamentos', function(req, res) {
+            console.log('');
+            res.send("ok");
         });
-    });
+        app.post('/pagamentos/pagamento', function(req, res) {
+            let pagamento = req.body;
+            console.log("processano um novo pagamento");
 
-}
+            let connection = app.persistencia.connectionFactory();
+            let pagamentoDao = new app.persistencia.PagamentoDao(connection);
 
-//curl http://localhost:3000/pagamentos/pagamento -X POST -v -H "Content-type:application/json" -d @./files/pagamento.json;
+            pagamento.status = "CRIADO"
+            pagamento.data = new Date();
+
+            pagamentoDao.salva(pagamento, function(erro, resultado) {
+                console.log('pagamento criado ' + resultado);
+                console.log(erro)
+                res.json(pagamento);
+            });
+        });
+
+    }
+    // teste
+    //curl http://localhost:3000/pagamentos/pagamento -X POST -v -H "Content-type:application/json" -d @./files/pagamento.json;
